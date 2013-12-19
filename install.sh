e9ff1f793fa5876d3e2481f5fe9876f35ae82313
Alexis Simonnet <lekhder.aissam@gmail.com>
2013-12-19 16:38:47 +0100

    ajout de install.sh pour recuperer les librairies necessaires

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#Pour les codes
+sudo apt-get install kdevelop
+sudo apt-get install scons
+sudo apt-get install g++-4.4
+sudo ln -s /usr/bin/g++-4.4 /usr/bin/g++
+sudo apt-get install gcc-4.4
+sudo ln -s /usr/bin/gcc-4.4 /usr/bin/gcc
+sudo apt-get install python
+sudo apt-get install python-dev
+sudo apt-get install libgsl0-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install libxml++2
+sudo apt-get install libpython-devel
+sudo apt-get install swig
+sudo apt-get install libboost-dev
+sudo apt-get install python-all-dev
+#sudo apt-get install libg2c            #Trouver une autre source
+sudo apt-get install openmpi-bin
+sudo apt-get install libopenmpi-dev
+
+# Pour Metil
+sudo apt-get install libreadline-dev
+sudo apt-get install cmake
+sudo apt-get install libqt4-core
+sudo apt-get install qt4-qmake
+sudo apt-get install libhdf5-serial-dev
+#sudo apt-get install qt                #Pas de paquet correspondant
+sudo apt-get install qtcreator
+
+# Pour la visu
+sudo apt-get install zlib1g-dev
+sudo apt-get install libglew-dev
+sudo apt-get install libfcgi-dev
+
+# Pour l'interface
+sudo apt-get install coffeescript
+
+# Pour cuda
+sudo apt-get install libxi-dev
+sudo apt-get install libxmu-dev
+sudo apt-get install freeglut3-dev
+
